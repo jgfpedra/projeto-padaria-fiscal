789c68258c31c83a10cb233f11ad989f9fcb4d65
jgfpedra <j248349@dac.unicamp.br>
2024-05-02 06:00:58 -0300

    Adicao tipo venda

--- cieloScript.py
+++ cieloScript.py
@@ -17,7 +17,7 @@ for rownum in reversed(range(5, sh.nrows)):
     d2 = datetime.strptime(sh.cell(rownum, 1).value, "%d/%m/%Y")
     if((abs((d1 - d2).days) == 5)):
         somaValor += (locale.atof(sh.cell(rownum, 7).value.strip("R$"))/100)
-        dicionario[sh.cell(rownum, 1).value] = [sh.cell(rownum, 3).value, sh.cell(rownum, 11).value, somaValor]
+        dicionario[sh.cell(rownum, 1).value] = [sh.cell(rownum, 3).value, sh.cell(rownum, 4).value, sh.cell(rownum, 11).value, somaValor]
 
 df = pd.DataFrame(data=dicionario)
 df = (df.T)
