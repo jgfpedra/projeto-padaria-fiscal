2c0f54484a192e42d8db76a2745a58de1c609ce6
jgfpedra <j248349@dac.unicamp.br>
2024-05-09 10:36:52 -0300

    Mudanca na Logica

--- cieloScript.py
+++ cieloScript.py
@@ -3,50 +3,38 @@ from datetime import datetime, timedelta
 import locale
 import pandas as pd
 
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
-
 book = xlrd.open_workbook("venda.xls")
 sh = book.sheet_by_index(0)
 
 locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 
 valoresComDias = []
-diaFeriados = []
+temp = [0, 0]
+pagamentos = [0, 1, 2, 3, 4]
+bandeiras = ["Visa", "Mastercard"]
+tiposVenda = ["Crédito à vista", "Crédito conversor moedas"]
 
-somaValor = 0 
-quantidadeFeriado = 0
-
-finalSemana = [5, 6]
-
-possuiFeriado = input("Houve algum feriado entre o periodo de dias da tabela? (s | n)\nEntrada: ")
-
-if possuiFeriado == 's':
-    quantidadeFeriado = int(input("Quantos dias foram? (Somente numero inteiro)\n"))
-    while(quantidadeFeriado != 0):
-        diaFeriados.append(datetime.strptime(input("Qual o dia do feriado? (XX/XX/XXXX): "), "%d/%m/%Y"))
-        quantidadeFeriado -= 1
-
+prev = float('-inf')
+chave = 0
+isConversor = False
 
 for rownum in reversed(range(5, sh.nrows)):
     d1 = datetime.strptime(sh.cell(rownum, 11).value, "%d/%m/%Y")
     d2 = datetime.strptime(sh.cell(rownum, 1).value, "%d/%m/%Y")
 
-    diasTotais = (abs((d1 - d2).days))
+    bandeira = sh.cell(rownum, 3).value
+    tipoVenda = sh.cell(rownum, 4).value
 
-    for date in daterange(d2, d1):
-        if date.weekday() in finalSemana:
-            diasTotais -= 1
-        if date in diaFeriados:
-            diasTotais -= 1
+    diferencaDias = (abs((d2 - d1).days))
 
-    if possuiFeriado == 's':
-        diasTotais = diasTotais - quantidadeFeriado
+    if(d1.day > prev):
+        prev = d1.day
+        somaValor = 0
 
-    if(diasTotais == 5):
-        somaValor += (locale.atof(sh.cell(rownum, 7).value.strip("R$"))/100)
-        valoresComDias.append([sh.cell(rownum, 1).value, sh.cell(rownum, 3).value, sh.cell(rownum, 4).value, sh.cell(rownum, 11).value, somaValor])
+    if((diferencaDias < 8) 
+       & (tipoVenda in tiposVenda)
+       & (bandeira in bandeiras)):
+            valoresComDias.append([sh.cell(rownum, 1).value, sh.cell(rownum, 3).value, sh.cell(rownum, 4).value, sh.cell(rownum, 11).value, (locale.atof(sh.cell(rownum, 7).value.strip("R$"))/100)])
 
 df = pd.DataFrame(data=valoresComDias)
 df.to_excel('resumo.xlsx')
---
+++ cieloScriptValores.py
@@ -0,0 +1,48 @@
+import xlrd
+from datetime import datetime, timedelta
+import locale
+import pandas as pd
+
+book = xlrd.open_workbook("venda.xls")
+sh = book.sheet_by_index(0)
+
+locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+
+valoresComDias = []
+temp = [0, 0]
+pagamentos = [0, 1, 2, 3, 4]
+bandeiras = ["Visa", "Mastercard"]
+tiposVenda = ["Crédito à vista", "Crédito conversor moedas"]
+
+prev = float('-inf')
+chave = 0
+isConversor = False
+
+for rownum in reversed(range(5, sh.nrows)):
+    d1 = datetime.strptime(sh.cell(rownum, 11).value, "%d/%m/%Y")
+    d2 = datetime.strptime(sh.cell(rownum, 1).value, "%d/%m/%Y")
+
+    bandeira = sh.cell(rownum, 3).value
+    tipoVenda = sh.cell(rownum, 4).value
+
+    diferencaDias = (abs((d2 - d1).days))
+
+    if((diferencaDias < 8) 
+       & (tipoVenda in tiposVenda)
+       & (bandeira in bandeiras)):
+
+        if(d1.day > prev):
+            prev = d1.day
+            valoresComDias.append([sh.cell(rownum, 1).value, sh.cell(rownum, 3).value, tiposVenda[0], sh.cell(rownum, 11).value, temp[0]])
+            if(temp[1] != 0):
+                valoresComDias.append([sh.cell(rownum, 1).value, sh.cell(rownum, 3).value, tiposVenda[1], sh.cell(rownum, 11).value, temp[1]])
+            somaValor = 0
+            temp = [0, 0]
+
+        if(tipoVenda == tiposVenda[0]):
+            temp[0] += (locale.atof(sh.cell(rownum, 7).value.strip("R$"))/100)
+        elif(tipoVenda == tiposVenda[1]):
+            temp[1] += (locale.atof(sh.cell(rownum, 7).value.strip("R$"))/100)
+
+df = pd.DataFrame(data=valoresComDias)
+df.to_excel('resumo.xlsx')
